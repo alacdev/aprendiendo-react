0828a6c615c2ae61d6ce1072b189bed60febe917
Hugo Alén Acuña <hugoaalen@gmail.com>
2024-04-09 20:40:59 +0200

    Primer día aprendiendo react

---
+++ projects/01-TwitterFollowSection/src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'alacdev',
+    name: 'Hugo Alén Acuña',
+    isFollowing: false
+  },
+  {
+    userName: 'PacoHdezs',
+    name: 'Paco Hdez',
+    isFollowing: true
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false
+  },
+  {
+    userName: 'pepelu',
+    name: 'Pepe Luis',
+    isFollowing: false
+  }
+]
+
+export function App () {
+  return (
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
+  )
+}
